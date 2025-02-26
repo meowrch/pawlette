f4b3c78725b6eef69ce355844e8424b0c2195dc4
DIMFLIX-OFFICIAL <dimflix.official@gmail.com>
2025-02-26 19:25:57 +0300

    Implemented cli interface for interaction with the script. Made applaying of themes. Wrote a readme file.

--- src/app.py
+++ src/app.py
@@ -1,11 +1,58 @@
 #!/usr/bin/env python3
+import argparse
+
+from loguru import logger
+
 import constants as cnst
-from utils.misc import create_dirs
+from theming.manager import ThemeManager
+
+
+def configure_argparser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description="Theme manager for Meowrch OS", prog="pawlette"
+    )
+    subparsers = parser.add_subparsers(
+        dest="command", required=True, title="subcommands", description="valid commands"
+    )
+
+    # get themes
+    subparsers.add_parser("get-themes", help="List all available themes")
+
+    # Apply theme
+    theme_parser = subparsers.add_parser("set-theme", help="Apply specified theme")
+    theme_parser.add_argument("theme_name", type=str, help="Name of theme to apply")
+    return parser
 
 
 def main() -> None:
-    # Создание каталогов, если их нет.
-    create_dirs(paths=[cnst.APP_DATA_DIR, cnst.APP_CACHE_DIR, cnst.APP_CONFIG_DIR])
+    manager = ThemeManager()
+
+    ##==> Создание каталогов, если их нет.
+    ##########################################
+    dirs_to_create = [
+        cnst.APP_DATA_DIR,
+        cnst.APP_CACHE_DIR,
+        cnst.APP_CONFIG_DIR,
+        cnst.THEMES_FOLDER,
+    ]
+
+    for p in dirs_to_create:
+        p.mkdir(parents=True, exist_ok=True)
+
+    ##==> Обработка аргументов
+    ##########################################
+    parser = configure_argparser()
+    args = parser.parse_args()
+
+    match args.command:
+        case "get-themes":
+            themes = manager.get_all_themes()
+            print("\n".join([i.name for i in themes]))
+        case "set-theme":
+            if args.theme_name:
+                manager.apply_theme(args.theme_name)
+        case _:
+            logger.warning(f'Command "{args.command}" not found!')
 
 
 if __name__ == "__main__":
