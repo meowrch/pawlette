63e79928f2e61aad9c229528ec89180779f33770
DIMFLIX-OFFICIAL <dimflix.official@gmail.com>
2025-02-26 22:49:27 +0300

    Added configuration parsing for pawlette from a .json file.

---
+++ src/config.py
@@ -0,0 +1,47 @@
+import json
+from pathlib import Path
+
+from loguru import logger
+
+import constants as cnst
+from schemas.config_struct import Config
+
+
+def generate_default_config():
+    cnst.APP_CONFIG_FILE.parent.mkdir(parents=True, exist_ok=True)
+    with open(cnst.APP_CONFIG_FILE, "w", encoding="utf-8") as f:
+        json.dump(
+            obj=cnst.DEFAULT_CONFIG,
+            fp=f,
+            indent=4,
+        )
+
+
+def load_config(path: Path) -> Config:
+    if not path.exists():
+        logger.warning(
+            f"Warning: The config file '{path}' was not found. Using default config."
+        )
+        return Config(**cnst.DEFAULT_CONFIG)
+
+    try:
+        with open(path) as f:
+            config_dict = json.load(f)
+
+            if "comment_styles" in config_dict:
+                for comment_format in cnst.COMMENT_FORMATS:
+                    if comment_format not in config_dict["comment_styles"]:
+                        config_dict["comment_styles"][comment_format] = (
+                            cnst.COMMENT_FORMATS[comment_format]
+                        )
+
+            config = Config(**config_dict)
+            return config
+    except Exception:
+        logger.warning(
+            f"Warning: The config file '{path}' is invalid. Using default config."
+        )
+        return Config(**cnst.DEFAULT_CONFIG)
+
+
+cfg: Config = load_config(cnst.APP_CONFIG_FILE)
--- src/constants.py
+++ src/constants.py
@@ -8,14 +8,32 @@ APPLICATION_NAME = "pawlette"
 XDG_DATA_HOME = Path(os.getenv("XDG_DATA_HOME", Path.home() / ".local" / "share"))
 XDG_CACHE_HOME = Path(os.getenv("XDG_CACHE_HOME", Path.home() / ".cache"))
 XDG_CONFIG_HOME = Path(os.getenv("XDG_CONFIG_HOME", Path.home() / ".config"))
+XDG_STATE_HOME = Path(os.getenv("XDG_STATE_HOME", Path.home() / ".local" / "state"))
 
 APP_FOLDER = Path(__file__).resolve().parent
 
 # Пути для данных, кэша и конфигураций приложения
+APP_CONFIG_DIR = XDG_CONFIG_HOME / APPLICATION_NAME
 APP_DATA_DIR = XDG_DATA_HOME / APPLICATION_NAME
 APP_CACHE_DIR = XDG_CACHE_HOME / APPLICATION_NAME
-APP_CONFIG_DIR = XDG_CONFIG_HOME / APPLICATION_NAME
+APP_BACKUP_DIR = XDG_STATE_HOME / APPLICATION_NAME / "backups"
 
-# Путь к темам (системный каталог)
+APP_CONFIG_FILE = APP_CONFIG_DIR / (APPLICATION_NAME + ".json")
+
 THEMES_FOLDER = APP_DATA_DIR / "themes"
 SYS_THEMES_FOLDER = Path(f"/usr/share/{APPLICATION_NAME}")
+
+
+# Форматы комментариев для разных расширений
+COMMENT_FORMATS = {
+    ".json": "//",
+    ".conf": "#",
+    ".yaml": "#",
+    ".toml": "#",
+}
+
+
+DEFAULT_CONFIG = {
+    "max_backups": 5,
+    "comment_styles": COMMENT_FORMATS,
+}
