195b8119f8aca9e036d268d6afb50f062e9fabb1
DIMFLIX <dimflix.official@gmail.com>
2025-03-31 20:55:23 +0300

    Added a logo for the theme

--- src/schemas/themes.py
+++ src/schemas/themes.py
@@ -4,12 +4,14 @@ from pydantic import BaseModel
 from pydantic import Field
 from pydantic import field_validator
 
+from constants import DEFAULT_THEME_LOGO
 from errors.themes import ThemeNotFound
 
 
 class Theme(BaseModel):
     name: str
     path: Path
+    theme_logo: Path = Field(default=None, exclude=True)
     wallpapers_folder: Path = Field(default=None, exclude=True)
     gtk_folder: Path = Field(default=None, exclude=True)
 
@@ -21,6 +23,10 @@ class Theme(BaseModel):
         super().__init__(**data)
         self.gtk_folder = self.path / "global" / "gtk-theme"
         self.wallpapers_folder = self.path / "global" / "wallpapers"
+
+        self.theme_logo = self.path / "logo.png"
+        if not self.theme_logo.exists():
+            self.theme_logo = DEFAULT_THEME_LOGO
 
     @field_validator("path")
     def validate_path(cls, v):
